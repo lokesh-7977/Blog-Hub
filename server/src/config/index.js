0c63df616bdc7c505fcdd32de74aac1156f6123f
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-06-25 12:41:38 +0530

    "added Configs"

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -3,5 +3,7 @@ dotenv.config();
 
 export const config = {
     PORT : process.env.PORT,
-    DB : process.env.DB_URL
+    DB : process.env.DB_URL,
+    JWT_SECRET : process.env.JWT_SECRET,
+    JWT_EXPIRY : process.env.JWT_EXPIRY
 }
